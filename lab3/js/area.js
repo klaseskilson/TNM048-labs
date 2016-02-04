63a1c0d1bfe346f31d0766b30826113e63d2861c
Klas Eskilson <klas.eskilson@gmail.com>
2016-02-04 17:55:49 +0100

    Improve formatting in area.js

--- lab3/js/area.js
+++ lab3/js/area.js
@@ -15,61 +15,61 @@ function area(data) {
 
     //Sets the scales
     var x = d3.time.scale().range([0, width]),
-            x2 = d3.time.scale().range([0, width]),
-            y = d3.scale.linear().range([height, 0]),
-            y2 = d3.scale.linear().range([height2, 0]);
+        x2 = d3.time.scale().range([0, width]),
+        y = d3.scale.linear().range([height, 0]),
+        y2 = d3.scale.linear().range([height2, 0]);
 
     //Sets the axis
     var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-            xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
-            yAxis = d3.svg.axis().scale(y).orient("left");
+        xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
+        yAxis = d3.svg.axis().scale(y).orient("left");
 
     //Assigns the brush to the small chart's x axis
     var brush = d3.svg.brush()
-            .x(x2)
-            .on("brush", brushFun);
+        .x(x2)
+        .on("brush", brushFun);
 
     //Creates the big chart
     var chart = d3.svg.area()
-            .interpolate("step")
-            .x(function (d) {
-                return 10;//Complete the code
-            })
-            .y0(height)
-            .y1(function (d) {
-                return 10;//Complete the code
-            });
+        .interpolate("step")
+        .x(function (d) {
+            return 10;//Complete the code
+        })
+        .y0(height)
+        .y1(function (d) {
+            return 10;//Complete the code
+        });
 
     //Creates the small chart
-        var area2 = d3.svg.area()
-            .interpolate("step")
-            .x(function (d) {
-                return 10;//Complete the code
-            })
-            .y0(height2)
-            .y1(function (d) {
-                return 10;//Complete the code
-            });
+    var area2 = d3.svg.area()
+        .interpolate("step")
+        .x(function (d) {
+            return 10;//Complete the code
+        })
+        .y0(height2)
+        .y1(function (d) {
+            return 10;//Complete the code
+        });
 
     //Assings the svg canvas to the area div
     var svg = d3.select("#area").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom);
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
 
     //Defines clip region
     svg.append("defs").append("clipPath")
-            .attr("id", "clip")
-            .append("rect")
-            .attr("width", width)
-            .attr("height", height);
+        .attr("id", "clip")
+        .append("rect")
+        .attr("width", width)
+        .attr("height", height);
 
     //Defines the focus area
     var focus = svg.append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     //Defines the context area
     var context = svg.append("g")
-            .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+        .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
     //Initializes the axis domains for the big chart
     x.domain([10,10]);//Complete the code
@@ -80,39 +80,39 @@ function area(data) {
 
     //Appends the big chart to the focus area
     focus.append("path")
-            .datum(data)
-            .attr("clip-path", "url(#clip)")
-            .attr("d", chart);
+        .datum(data)
+        .attr("clip-path", "url(#clip)")
+        .attr("d", chart);
 
     //Appends the x axis
     focus.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
     //Appends the y axis
     focus.append("g")
-            .attr("class", "y axis")
-            .call(yAxis);
+        .attr("class", "y axis")
+        .call(yAxis);
 
     //Appends the small chart to the focus area
     context.append("path")
-            .datum(data)
-            .attr("d", area2);
+        .datum(data)
+        .attr("d", area2);
 
     //Appends the x axis
     context.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height2 + ")")
-            .call(xAxis2);
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height2 + ")")
+        .call(xAxis2);
 
     //Appends the brush
     context.append("g")
-            .attr("class", "x brush")
-            .call(brush)
-            .selectAll("rect")
-            .attr("y", -6)
-            .attr("height", height2 + 7);
+        .attr("class", "x brush")
+        .call(brush)
+        .selectAll("rect")
+        .attr("y", -6)
+        .attr("height", height2 + 7);
 
 
 
