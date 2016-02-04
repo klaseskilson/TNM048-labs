5a0f1a2ff8bd170461ee0275f2eace9bedd3a54d
Klas Eskilson <klas.eskilson@gmail.com>
2016-02-04 18:41:52 +0100

    Add F+C area chart

--- lab3/js/area.js
+++ lab3/js/area.js
@@ -5,13 +5,13 @@ function area(data) {
     var areaDiv = $("#area");
 
     var margin = {top: 100, right: 40, bottom: 100, left: 40},
-    margin2 = {top: areaDiv.height() - 50, right: 40, bottom: 20, left: 40},
-    width = areaDiv.width() - margin.left - margin.right,
-            height = areaDiv.height() - margin.top - margin.bottom,
-            height2 = areaDiv.height() - margin2.top - margin2.bottom;
+        margin2 = {top: areaDiv.height() - 50, right: 40, bottom: 20, left: 40},
+        width = areaDiv.width() - margin.left - margin.right,
+        height = areaDiv.height() - margin.top - margin.bottom,
+        height2 = areaDiv.height() - margin2.top - margin2.bottom;
 
-    //Sets the data format
-    var format = d3.time.format.utc("");//Complete the code
+    //Sets the data format 2016-01-13T17:01:29.450Z
+    var format = d3.time.format.utc("%Y-%m-%dT%H:%M:%S.%LZ").parse;
 
     //Sets the scales
     var x = d3.time.scale().range([0, width]),
@@ -33,22 +33,22 @@ function area(data) {
     var chart = d3.svg.area()
         .interpolate("step")
         .x(function (d) {
-            return 10;//Complete the code
+            return x(format(d.time));
         })
         .y0(height)
         .y1(function (d) {
-            return 10;//Complete the code
+            return y(parseFloat(d.mag));
         });
 
     //Creates the small chart
     var area2 = d3.svg.area()
         .interpolate("step")
         .x(function (d) {
-            return 10;//Complete the code
+            return x2(format(d.time));//Complete the code
         })
         .y0(height2)
         .y1(function (d) {
-            return 10;//Complete the code
+            return y2(parseFloat(d.mag));
         });
 
     //Assings the svg canvas to the area div
@@ -71,9 +71,9 @@ function area(data) {
     var context = svg.append("g")
         .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-    //Initializes the axis domains for the big chart
-    x.domain([10,10]);//Complete the code
-    y.domain([4, 10]);//Complete the code
+    //Initializes the axis domains for the big char
+    x.domain(d3.extent(data.map(function(d) { return format(d.time); })));
+    y.domain([0, d3.max(data.map(function(d) { return parseFloat(d.mag); }))]);
     //Initializes the axis domains for the small chart
     x2.domain(x.domain());
     y2.domain(y.domain());
@@ -113,9 +113,6 @@ function area(data) {
         .selectAll("rect")
         .attr("y", -6)
         .attr("height", height2 + 7);
-
-
-
 
     //Method for brushing
     function brushFun() {
